e25c85aa61df8d29bcbe2890172a617ff7af205d
Evgeniy Zabolotniy <eugenezabolotniy@yandex.ru>
2022-12-29 11:18:19 +0800

    Array article was hidden

--- Sources/EzabolotniyGithubIo/main.swift
+++ Sources/EzabolotniyGithubIo/main.swift
@@ -7,8 +7,8 @@ import SplashPublishPlugin
 struct EzabolotniyGithubIo: Website {
   enum SectionID: String, WebsiteSectionID {
     // Add the sections that you want your website to contain here:
-    case camera
-    case datastructures
+//    case camera
+    case structures
     case interview
     case about
   }
